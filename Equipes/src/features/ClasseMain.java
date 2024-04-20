0c52f06b30da8e0ab0217f7b8151e95a8fa6a897
EddyHunter85 <EddyHunter85@hotmail.com>
2024-04-19 23:27:00 -0300

    Criada as primeiras classes

---
+++ Equipes/src/features/ClasseMain.java
@@ -0,0 +1,14 @@
+package features;
+
+public class ClasseMain {
+
+	public static void main(String[] args) {
+		Entrada e = new Entrada();
+		
+		e.setTexto("Escreva algo no terminal");
+		
+		System.out.println(e.getTexto());
+
+	}
+
+}
