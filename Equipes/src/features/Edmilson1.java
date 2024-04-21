0fba6fc2e24e6e4b167338dcf12faf1d33a2bcca
EdmilsonSoares <EdmilsonSoares85@hotmail.com>
2024-04-21 05:43:19 -0300

    Criado calsse Edmilson1

---
+++ Equipes/src/features/Edmilson1.java
@@ -0,0 +1,16 @@
+package features;
+
+public class Edmilson1 {
+	private String teste1;
+
+	public String getTeste1() {
+		return teste1;
+	}
+
+	public void setTeste1(String teste1) {
+		this.teste1 = teste1;
+	}
+	
+	
+
+}
